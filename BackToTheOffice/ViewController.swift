59fbe5efb38caa93f679e178e0eabbbb2f8be66d
Procorporate <procorporate@inbox.ru>
2020-09-09 16:52:03 +0300

    Start project

---
+++ BackToTheOffice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackToTheOffice
+//
+//  Created by Roman Egorov on 04.09.2020.
+//  Copyright © 2020 Roman Egorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
