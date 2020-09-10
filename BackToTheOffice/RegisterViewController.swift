08bf5eafe82f89e845dd3b4b443ac5ab6dd3a56a
Procorporate <procorporate@inbox.ru>
2020-09-11 00:09:15 +0300

    Все нужные элементы привязаны к коду, побеждена проблема с отображением элементов на  5, 5S, SE (надо смотреть в симуляторе или на реальном устройстве), реализована возможность ставить и убирать галочку в поле согласия с условиями приложения

--- BackToTheOffice/RegisterViewController.swift
+++ BackToTheOffice/RegisterViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    var blueSquareIsMarked = false
+    
+    
+    @IBOutlet weak var createAccountTopConstraint: NSLayoutConstraint!
 
+    @IBOutlet weak var createAccountButtonBottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var firstNameTF: UITextField!
+    
+    @IBOutlet weak var lastNameTF: UITextField!
+    
+    @IBOutlet weak var EmailTF: UITextField!
+    
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    @IBOutlet weak var blueSquareImage: UIImageView!
+    
+    @IBOutlet weak var iAgreeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Меняем значения некоторых констрейнтов, если приложение запущено на Айфоне SE, 5, 5S
+        if UIScreen.main.bounds.height == 568.0 {
+           print("Айфон SE детектед")
+            
+            createAccountTopConstraint.constant = 10
+            createAccountButtonBottomConstraint.constant = 10
+            iAgreeLabel.text = "By signing up, I agree to the application"
+        }
+        
+        
+        
     }
 
+    @IBAction func privacyPolicyButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func termsOfServiceButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func blueSquareButtonTapped(_ sender: UIButton) {
+        
+        blueSquareIsMarked = !blueSquareIsMarked
+        
+        if blueSquareIsMarked {
+            blueSquareImage.image = UIImage(named: "blueSquareMarked")
+        } else {
+            blueSquareImage.image = UIImage(named: "blueSquareClear")
+        }
+        
+        
+        
+    }
+    
+    @IBAction func createAccountButtonTapped(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
