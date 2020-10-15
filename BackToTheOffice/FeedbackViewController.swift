6f26af649c2e02ad57c5c0fdafd42321d5a5e342
Procorporate <procorporate@inbox.ru>
2020-10-15 15:33:27 +0300

    В профиле коллеги по команде, при попытке перейти к одному из мессенджеров или в почту:    --- Если на устройстве соответствующий мессенджер не установлен, приложение покажет пользователю соответствующий AlertController, с возможностью скопировать в буфер обмена соответствующий UID

--- BackToTheOffice/FeedbackViewController.swift
+++ BackToTheOffice/FeedbackViewController.swift
@@ -39,26 +39,26 @@ class FeedbackViewController: UIViewController {
     
     @IBAction func telegramButonTapped(_ sender: UIButton) {
         
-        openApplicationOrPresentAC(appURL: "tg://resolve?domain=@Sergey_Efimov12", titleAC: "Приложение \"Telegram\" не установлено на вашем устройстве", messageAC: "")
+        openApplicationOrPresentAC(appURL: "tg://resolve?domain=", userID: "@Sergey_Efimov12", titleAC: "Приложение \"Telegram\" не установлено на вашем устройстве")
     }
     
     @IBAction func whatsAppButonTapped(_ sender: UIButton) {
-        openApplicationOrPresentAC(appURL: "whatsapp://send/?phone=79859704545", titleAC: "Приложение \"WhatsApp\" не установлено на вашем устройстве", messageAC: "")
+        openApplicationOrPresentAC(appURL: "whatsapp://send/?phone=", userID: "79859704545", titleAC: "Приложение \"WhatsApp\" не установлено на вашем устройстве")
     }
     
     @IBAction func viberButonTapped(_ sender: UIButton) {
         
-        openApplicationOrPresentAC(appURL: "viber://contact?number=+79859704544", titleAC: "Приложение \"Viber\" не установлено на вашем устройстве", messageAC: "")
+        openApplicationOrPresentAC(appURL: "viber://contact?number=", userID: "+79859704544", titleAC: "Приложение \"Viber\" не установлено на вашем устройстве")
     }
     
     @IBAction func skypeButonTapped(_ sender: UIButton) {
-        openApplicationOrPresentAC(appURL: "skype:live:3bd7c6f2431c06a1?chat", titleAC: "Приложение \"Skype\" не установлено на вашем устройстве", messageAC: "")
+        openApplicationOrPresentAC(appURL: "skype:", userID: "live:3bd7c6f2431c06a1", titleAC: "Приложение \"Skype\" не установлено на вашем устройстве")
         //live:3bd7c6f2431c06a1 - это userID
     }
     
     @IBAction func emailButonTapped(_ sender: UIButton) {
         
-        openApplicationOrPresentAC(appURL: "mailto:procorporate@inbox.ru", titleAC: "Приложение \"Почта\" не установлено на вашем устройстве", messageAC: "Для отправки письма, воспользуйтесь сторонним приложением.\n Электронный адрес выбранного контакта: procorporate@inbox.ru")
+        openApplicationOrPresentAC(appURL: "mailto:", userID: "procorporate@inbox.ru", titleAC: "Приложение \"Почта\" не установлено на вашем устройстве")
     }
     
     @IBAction func smsButonTapped(_ sender: UIButton) {
@@ -66,21 +66,53 @@ class FeedbackViewController: UIViewController {
     }
     
     @IBAction func slackButonTapped(_ sender: UIButton) {
-        openApplicationOrPresentAC(appURL: "slack://user?team=teambbworkspace&id=Roman1983", titleAC: "Приложение \"Slack\" не установлено на вашем устройстве", messageAC: "")
+        openApplicationOrPresentAC(appURL: "slack://user?team=teambbworkspace&id=", userID: "Roman1983", titleAC: "Приложение \"Slack\" не установлено на вашем устройстве")
     }
     
-    private func openApplicationOrPresentAC (appURL: String, titleAC: String, messageAC: String) {
-        if UIApplication.shared.canOpenURL((URL(string: appURL)!)) {
+    private func openApplicationOrPresentAC (appURL: String, userID: String, titleAC: String) {
+        
+        var urlString = appURL + userID
+        var messageAC = ""
+        
+        switch appURL {
+        case "tg://resolve?domain=":
+            messageAC = "Идентификатор этого пользователя в Telegram \(userID)"
+        case "whatsapp://send/?phone=":
+            messageAC = "Номер телефона этого пользователя \(userID)"
+        case "viber://contact?number=":
+            messageAC = "Номер телефона этого пользователя \(userID)"
+        case "skype:":
+            urlString = appURL + userID + "?chat"
+            messageAC = "Идентификатор этого пользователя в Skype \(userID)"
+        case "mailto:":
+            messageAC = "Email этого пользователя \(userID)"
+        case "slack://user?team=teambbworkspace&id=":
+            messageAC = "Идентификатор этого пользователя в Slack \(userID)"
+        
+        default:
+            break
+        }
+        
+        if UIApplication.shared.canOpenURL((URL(string: urlString)!)) {
             
-            UIApplication.shared.open(URL(string: appURL)!)
+            UIApplication.shared.open(URL(string: urlString)!)
             
         } else {
             
             let alertController = UIAlertController(title: titleAC, message: messageAC, preferredStyle: .alert)
             
-            let alertAction = UIAlertAction(title: "Ok", style: .default)
+            alertController.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            alertController.view.layer.cornerRadius = 12
             
-            alertController.addAction(alertAction)
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            let copyUserIdAction = UIAlertAction(title: "Копировать", style: .default) { action in
+                
+                UIPasteboard.general.string = userID
+                
+            }
+            
+            alertController.addAction(okAction)
+            alertController.addAction(copyUserIdAction)
             
             present(alertController, animated: true)
         }
